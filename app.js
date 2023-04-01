789db6fb3d3079812b6bb8db8bd432e735481e63
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-04-01 19:45:28 +0200

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const team_routes = require('./routes/team');
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 app.use('/api',team_routes);
 
 module.exports = app;
