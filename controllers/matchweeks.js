e1ab07bda3128eb50102165fe75675f88f027235
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-10-24 19:00:49 +0200

    Return logos on getMatchWeeks controller

--- controllers/matchweeks.js
+++ controllers/matchweeks.js
@@ -2,11 +2,25 @@
 
 // const validator = require('validator');
 const { matchWeeksModel } = require('../models/matchweeks');
+const { TeamModelA, TeamModelB } = require('../models/team');
 
 const controller = {
     getMatchWeeks: async (req, res) =>{
         try{
             const matchWeeks = await matchWeeksModel.find();
+            for (let week of matchWeeks) {
+                for (let hourMatch of week.matches) {
+                  const localA = await TeamModelA.findOne({ team: hourMatch.groupA.local });
+                  const visitorA = await TeamModelA.findOne({ team: hourMatch.groupA.visitor });
+                  hourMatch.groupA["localLogo"] = localA.logo ? localA.logo : "https://res.cloudinary.com/dzd68sxue/image/upload/v1695396332/WEBP/default-bnoacd-1_qnmcps.webp";
+                  hourMatch.groupA["visitorLogo"] = visitorA.logo ? visitorA.logo : "https://res.cloudinary.com/dzd68sxue/image/upload/v1695396332/WEBP/default-bnoacd-1_qnmcps.webp";
+                  
+                  const localB = await TeamModelB.findOne({ team: hourMatch.groupB.local });
+                  const visitorB = await TeamModelB.findOne({ team: hourMatch.groupB.visitor });
+                  hourMatch.groupB["localLogo"] = localB.logo ? localB.logo : "https://res.cloudinary.com/dzd68sxue/image/upload/v1695396332/WEBP/default-bnoacd-1_qnmcps.webp";
+                  hourMatch.groupB["visitorLogo"] = visitorB.logo ? visitorB.logo : "https://res.cloudinary.com/dzd68sxue/image/upload/v1695396332/WEBP/default-bnoacd-1_qnmcps.webp";
+                }
+              }
 
             return !matchWeeks ?
                     res.status(400).send({
@@ -18,6 +32,7 @@ const controller = {
                     matchWeeks: matchWeeks
                     });
         }catch(err){
+            console.log(err);
             return res.status(500).send({
                 status: 'error',
                 message: 'Error al devolver jornadas.',
